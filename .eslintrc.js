4540d982bcf2391c66a465e3f9abda9cfced7904
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2020-12-26 12:56:18 -0300

    feat: Inicia setup node com typescript, eslint, express

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "arrow-parens": ["error", "always"],
+    "max-len": ["error", { code: 100, ignoreTemplateLiterals: true, ignoreComments: true }],
+    quotes: ["error", "double"],
+    "comma-dangle": [
+      2,
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "always-multiline",
+      },
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
