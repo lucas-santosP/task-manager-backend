50e9db7f14678a0f8b96f24de213dae5b98f9095
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-05-10 12:06:00 -0300

    feat: Improve database connection
    Allowing docker and local config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
+const DEFAULT_OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
 function connectDB() {
   const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-  mongoose.connect(
-    `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    },
-  );
+  if (DB_USER || DB_PASS || DB_HOST || DB_PORT || DB_NAME) {
+    mongoose.connect(
+      `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+      DEFAULT_OPTIONS,
+    );
+  } else {
+    mongoose.connect("mongodb://localhost:27017/task-manager", DEFAULT_OPTIONS);
+  }
 
   mongoose.connection.on("error", (error) => console.error("DB connection error", error));
-  mongoose.connection.once("open", () => console.log(`DB ${DB_NAME} connected`));
+  mongoose.connection.once("open", () => console.log("DB connected"));
   mongoose.connection.on("disconnected", () => console.error("DB disconnected"));
 }
 
