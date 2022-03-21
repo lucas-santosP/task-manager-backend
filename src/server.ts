72e87f2a957edac84009b62a2df3b77f5eb7548c
Lucas <lucas@computerscience.com.br>
2022-03-21 15:22:53 -0300

    feat: add initial page in the api

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,10 @@ import App from "./config/app";
 const app = new App().express;
 const PORT = process.env.PORT || 3333;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);
 });
