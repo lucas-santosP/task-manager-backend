338d356d3b65cab05dea5f7f85ca337db940afac
Lucas <lucas@computerscience.com.br>
2022-03-21 15:43:44 -0300

    test redeploy

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = new App().express;
 const PORT = process.env.PORT || 3333;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World " + process.env.ACCESS_TOKEN_SECRET || "");
 });
 
 app.listen(PORT, () => {
